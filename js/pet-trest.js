48edc19137e2b24f2bed9d834034a3d2f86bcb9d
Kevin Arnall <kevinjarnall@gmail.com>
2019-08-21 11:13:44 -0500

    now with cats, also randomizes the pictures

--- js/pet-trest.js
+++ js/pet-trest.js
@@ -10,29 +10,51 @@ var msnry = new Masonry('.grid', {
 var elems = [];
 var fragment = document.createDocumentFragment();
 
-
-var dogPictures = [];
-
 $.get("https://dog.ceo/api/breeds/image/random/50").done(function (data) {
-    dogPictures = data;
     for (var i = 0; i < 50; i++) {
         var elem = getItemElement();
-        elem.innerHTML += "<img height='100%' width='100%' src=" + dogPictures.message[i] + ">";
+        elem.innerHTML += "<img height='100%' width='100%' src=" + getCatUrl() + ">";
         fragment.appendChild(elem);
         elems.push(elem);
     }
-    // append elements to container
+    for (var i = 0; i < 50; i++) {
+        var elem = getItemElement();
+        elem.innerHTML += "<img height='100%' width='100%' src=" + data.message[i] + ">";
+        fragment.appendChild(elem);
+        elems.push(elem);
+    }
+    shuffleArray(elems);
     grid.appendChild(fragment);
     msnry.appended(elems);
 });
+
+function getCatUrl() {
+    var urlbase = "https://cataas.com/cat?";
+    var randnum = Math.floor(Math.random() * 1000);
+    var url = urlbase.concat(randnum.toString());
+    return url;
+}
 
 function getItemElement() {
     var elem = document.createElement('div');
     var wRand = Math.random();
     var hRand = Math.random();
-    var widthClass = wRand > 0.8 ? 'grid-item--width3' : wRand > 0.6 ? 'grid-item--width2' : '';
+    var widthClass = wRand > 0.99 ? 'grid-item--width3' : wRand > 0.9 ? 'grid-item--width2' : '';
     var heightClass = hRand > 0.85 ? 'grid-item--height4' : hRand > 0.6 ? 'grid-item--height3' : hRand > 0.35 ? 'grid-item--height2' : '';
 
     elem.className = 'grid-item ' + widthClass + ' ' + heightClass;
     return elem;
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
